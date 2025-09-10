fae01ce7f141163753e75d1c0c621bf5522a9035
Abhiroop CVK <hermitdudz@gmail.com>
2025-09-10 10:30:24 -0700

    updated projectID to latest

--- basic.config.ts
+++ basic.config.ts
@@ -1,78 +1,64 @@
-
-// Basic Project Configuration
-// see  the docs for more info: https://docs.basic.tech
-export const config = {
-	name: "loca-chat",
-	project_id: "ffc3d25c-10c1-49d8-a84b-a0a6bf742a66"
-};
-
 export const schema = {
-		"project_id": "ffc3d25c-10c1-49d8-a84b-a0a6bf742a66",
-		"tables": {
-			"chats": {
-				"fields": {
-					"created_at": {
-						"type": "string"
-					},
-					"title": {
-						"type": "string"
-					}
+	"tables": {
+		"chats": {
+			"type": "collection",
+			"fields": {
+				"title": {
+					"type": "string"
 				},
-				"type": "collection"
-			},
-			"messages": {
-				"fields": {
-					"chat_id": {
-						"type": "string"
-					},
-					"content": {
-						"type": "string"
-					},
-					"created_at": {
-						"type": "string"
-					},
-					"role": {
-						"type": "string"
-					}
-				},
-				"type": "collection"
-			},
-			"notes": {
-				"description": "notes table",
-				"fields": {
-					"text": {
-						"description": "main text value",
-						"type": "string"
-					}
-				},
-				"origin": {
-					"project_id": "bc1f568d-62f5-437f-af3a-398d90db58e6",
-					"table": "notes",
-					"type": "reference"
-				},
-				"type": "collection"
-			},
-			"tasks": {
-				"fields": {
-					"completed": {
-						"type": "boolean"
-					},
-					"description": {
-						"type": "string"
-					},
-					"name": {
-						"type": "string"
-					}
-				},
-				"name": "tasks",
-				"origin": {
-					"project_id": "701b11bc-59a8-45b5-8148-7184d7733e5b",
-					"table": "tasks",
-					"type": "reference"
-				},
-				"type": "collection"
+				"created_at": {
+					"type": "string"
+				}
 			}
 		},
-		"version": 2
-	}
-	;
+		"todos": {
+			"type": "collection",
+			"fields": {
+				"title": {
+					"type": "string",
+					"indexed": true
+				},
+				"dueDate": {
+					"type": "string",
+					"indexed": true
+				},
+				"priority": {
+					"type": "string",
+					"indexed": true
+				},
+				"completed": {
+					"type": "boolean",
+					"indexed": true
+				},
+				"description": {
+					"type": "string",
+					"indexed": true
+				}
+			},
+			"origin": {
+				"type": "reference",
+				"table": "todos",
+				"project_id": "9412a48d-ea6d-4ed0-b8d4-924364324d4f"
+			}
+		},
+		"messages": {
+			"type": "collection",
+			"fields": {
+				"role": {
+					"type": "string"
+				},
+				"chat_id": {
+					"type": "string"
+				},
+				"content": {
+					"type": "string"
+				},
+				"created_at": {
+					"type": "string"
+				}
+			}
+		}
+	},
+	"version": 3,
+	"project_id": "444f64d1-a385-44c1-8bad-d7298ec465d2"
+}
